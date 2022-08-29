625c5f60c78117ec483f1614a038e9eee65cb955
jackiezkl <jackiezkl@users.noreply.github.com>
2022-08-28 23:16:02 -0500

    Update parse_current.py

--- parse_current.py
+++ parse_current.py
@@ -1,11 +1,38 @@
-import os,sys,stem,time,pygeoip,geoip.database
+import os, sys, stem, time, linecache, pygeoip
+import geoip.database
 from stem.descriptor import DocumentHandler, parse_file
+from stem.descriptor.remote import DescriptorDownloader
 
 GEOIP_FILENAME = "GeoLite2-City.mmdb"
 geoip_reader = None
 
+def download_consensus():
+  downloader = DescriptorDownloader()
+  consensus = downloader.get_consensus(document_handler = DocumentHandler.DOCUMENT).run()[0]
+
+  with open('/tmp/consensus_dump', 'w') as descriptor_file:
+    descriptor_file.write(str(consensus))
+
+def filecopy():
+  fifth_line = linecache.getline('/tmp/consensus_dump',4).split()
+  commd = "cp /tmp/consensus_dump ./data/"+fifth_line[1]+"_"+fifth_line[2]
+  os.system(commd)
+
 def main():
-  
+  try:
+    download_consensus()
+    filecopy()
+  except Exception:
+    try:
+      download_consensus()
+      filecopy()
+    except Exception:
+        try:
+          download_consensus()
+          filecopy()
+        except Exception:
+          pass
+
 if __name__=='__main__':
   # Make sure we have a GeoIP database (maxmind)
   if not os.path.isfile(GEOIP_FILENAME):
@@ -13,3 +40,9 @@ if __name__=='__main__':
     print("Get the Maxmind city database here:")
     print("-> https://dev.maxmind.com/geoip/legacy/geolite")
     sys.exit(1)
+  geoip_reader = geoip2.database.Reader('./%s' % GEOIP_FILENAME)
+  
+  if not os.path.isdir("./data"):
+    os.mkdir("./data")
+  
+  main()
