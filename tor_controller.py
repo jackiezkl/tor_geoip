e678924fe204741a47216d61629aac07caec43e2
jackiezkl <jackiezkl@users.noreply.github.com>
2022-09-19 21:33:53 -0500

    Update tor_controller.py

--- tor_controller.py
+++ tor_controller.py
@@ -29,5 +29,5 @@ if __name__ == '__main__':
 
   print("Tor is changing circuit...")
   controller.new_circuit()
-  print(controller.get_circuit())
+  print(controller.get_circuit(20))
   controller.close()
