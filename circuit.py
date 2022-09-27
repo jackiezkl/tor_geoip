fbc3e35746d9eb2de52dd899ad123701d2875093
jackiezkl <jackiezkl@users.noreply.github.com>
2022-09-27 11:13:46 -0500

    Update circuit.py

--- circuit.py
+++ circuit.py
@@ -1,21 +1,38 @@
 from stem import CircStatus
 from stem.control import Controller
+import pygeoip
+import geoip2.database
 
-with Controller.from_port(port = 9051) as controller:
-  controller.authenticate()
+GEOIP_FILENAME = "GeoLite2-City.mmdb"
+geoip_reader = None
 
-  for circ in sorted(controller.get_circuits()):
-    if circ.status != CircStatus.BUILT:
-      continue
 
-    print("")
-    print("Circuit %s (%s)" % (circ.id, circ.purpose))
 
-    for i, entry in enumerate(circ.path):
-      div = '+' if (i == len(circ.path) - 1) else '|'
-      fingerprint, nickname = entry
+def geo_ip_lookup(ip_address):
+    record = geoip_reader.city(ip_address)
+    if record is None:
+        return ("unknown")
+    return (record.country.iso_code)
 
-      desc = controller.get_network_status(fingerprint, None)
-      address = desc.address if desc else 'unknown'
+if __name__ == '__main__':
+  geoip_reader = geoip2.database.Reader('./%s' % GEOIP_FILENAME)
+  
+  with Controller.from_port(port = 9051) as controller:
+    controller.authenticate()
 
-      print(" %s- %s (%s, %s)" % (div, fingerprint, nickname, address))
+    for circ in sorted(controller.get_circuits()):
+      if circ.status != CircStatus.BUILT:
+        continue
+
+      print("")
+      print("Circuit %s (%s)" % (circ.id, circ.purpose))
+
+      for i, entry in enumerate(circ.path):
+        div = '+' if (i == len(circ.path) - 1) else '|'
+        fingerprint, nickname = entry
+
+        desc = controller.get_network_status(fingerprint, None)
+        address = desc.address if desc else 'unknown'
+        country = geo_ip_lookup(address)
+
+      print(" %s- %s (%s, %s, %s)" % (div, fingerprint, nickname, address, country))
