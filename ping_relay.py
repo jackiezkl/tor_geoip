51d7c01de9da6cb798905952c1f02dfc4a7b71cb
jackiezkl <jackiezkl@users.noreply.github.com>
2022-10-01 11:47:07 -0500

    Update ping_relay.py

--- ping_relay.py
+++ ping_relay.py
@@ -1,6 +1,11 @@
 import csv
 from ping3 import ping
 
+ping_result_filename = 'data/ping_result.csv'
+
+csv = open(ping_reuslt_filename, 'w+')
+csv.write('nickname,fingerprint,ip,latency\n')
+
 with open('data/latest_relays-2022-09-29-15:00:00.csv') as latest_relays:
   heading = next(latest_relays)
   
@@ -11,6 +16,6 @@ with open('data/latest_relays-2022-09-29-15:00:00.csv') as latest_relays:
     if 'E' in line[2]:
       if line[6] == 'US':
         try:
-          print(line[0]+': '+line[3]+', '+str(ping(line[3], unit='ms')))
+          csv.write("%s,%s,%s,%s\n" % (line[0],line[1],line[3],str(ping(line[3], unit='ms')))
         except Exception:
           continue
