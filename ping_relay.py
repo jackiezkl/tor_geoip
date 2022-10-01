f0239d868337f402ab579089a196fd1e7815f3dd
jackiezkl <jackiezkl@users.noreply.github.com>
2022-10-01 10:33:58 -0500

    Update ping_relay.py

--- ping_relay.py
+++ ping_relay.py
@@ -6,4 +6,5 @@ with open('data/latest_relays-2022-09-29-15:00:00.csv') as latest_relays:
   relay_reader = csv.reader(latest_relays)
   
   for row in relay_reader:
-    print(row)
+    line = row
+    print(line[0])
