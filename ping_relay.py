6e080aa7cf7d5d92c92cd7da0fea04fcb19d3647
jackiezkl <jackiezkl@users.noreply.github.com>
2022-10-01 10:37:32 -0500

    Update ping_relay.py

--- ping_relay.py
+++ ping_relay.py
@@ -7,6 +7,6 @@ with open('data/latest_relays-2022-09-29-15:00:00.csv') as latest_relays:
   
   for row in relay_reader:
     line = row
-    if 'E' in line[2] then:
-      if line[6] == 'US' then:
+    if 'E' in line[2]:
+      if line[6] == 'US':
       print(line[0]+':'+line[3])
