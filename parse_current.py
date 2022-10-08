650eea8e04e5340e8ffc60991d46b5190d11ee71
jackiezkl <jackiezkl@users.noreply.github.com>
2022-10-08 09:31:25 -0500

    Update parse_current.py

--- parse_current.py
+++ parse_current.py
@@ -164,8 +164,8 @@ def main():
   difference = end_time - start_time
   print("  [+] All process done! Total time spent: %s seconds" % str(difference))
 if __name__=='__main__':
-  geoip_reader = geoip2.database.Reader('/usr/share/GeoIP/%s' % GEOIP_FILENAME)
-  
+#   geoip_reader = geoip2.database.Reader('/usr/share/GeoIP/%s' % GEOIP_FILENAME)
+  geoip_reader = geoip2.database.Reader('./%s' % GEOIP_FILENAME)
   if not os.path.isdir("./data"):
     #os.system("rm -R ./data")
     os.mkdir("./data")
