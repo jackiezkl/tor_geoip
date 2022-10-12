adf7a23d0355ebbb35a99b4a30df374c6d6a97c7
jackiezkl <jackiezkl@users.noreply.github.com>
2022-10-11 22:42:01 -0500

    Update config_tor.py

--- config_tor.py
+++ config_tor.py
@@ -2,14 +2,17 @@ import csv
 
 def main():
   fields = []
-  rows = []
+  rows = ''
   
   with open ('./data/2022-10-08-14-00-00-ping_exit_result.csv') as exit_relays:
     relay_list_reader = csv.reader(exit_relays)
     fields = next(relay_list_reader)
     for row in relay_list_reader:
-      rows.append(row[1])
+      rows = rows + row + ','
   print(rows)
-
+  
+  filename = 
+  
+  
 if __name__ == '__main__':
   main()
