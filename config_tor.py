f3c3b39beb01642e8f3e57d4d99cf7e896a19ba2
jackiezkl <jackiezkl@users.noreply.github.com>
2022-10-12 09:00:16 -0500

    Update config_tor.py

--- config_tor.py
+++ config_tor.py
@@ -1,23 +1,31 @@
 import csv
 
-def main():
-  fields = []
+def extract_relay_fingerprints(path_to_ping_result_file)
   rows = ''
-  
-  with open ('./data/2022-10-08-14-00-00-ping_exit_result.csv') as exit_relays:
+  with open (path_to_ping_result_file) as exit_relays:
     relay_list_reader = csv.reader(exit_relays)
     fields = next(relay_list_reader)
     for row in relay_list_reader:
       rows = rows + row[1] + ','
   rows = rows[:-1]
-  print(rows)
+  exit_relays.close()
+  return rows
+
+def main():
+  entries = ''
+  middles = ''
+  exits = ''
+  
+  entires = extract_relay_fingerprints('./data/2022-10-08-14-00-00-ping_guard_result.csv')
+  middles = extract_relay_fingerprints('./data/2022-10-08-14-00-00-ping_middle_result.csv')
+  exits = extract_relay_fingerprints('./data/2022-10-08-14-00-00-ping_exit_result.csv')
   
   with open('torrc', 'w') as tor_config:
     tor_config.write('SOCKSPort 172.17.0.1:9050\n')
     tor_config.write('\n')
-    tor_config.write('EntryNodes {us} StrictNodes 1\n')
-    tor_config.write('MiddleNodes {us} StrictNodes 1\n')
-    tor_config.write('ExitNodes '+rows+'\n')
+    tor_config.write('EntryNodes '+guards+'\n')
+    tor_config.write('MiddleNodes '+middless+'\n')
+    tor_config.write('ExitNodes '+exits+'\n')
     tor_config.write('\n')
     tor_config.write('ControlPort 9051\n')
     
