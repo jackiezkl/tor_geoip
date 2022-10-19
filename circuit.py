0183e5db495a0974c5b6d52945f496ff5fed7330
jackiezkl <jackiezkl@users.noreply.github.com>
2022-10-19 11:35:19 -0500

    Update circuit.py

--- circuit.py
+++ circuit.py
@@ -13,12 +13,22 @@ geoip_reader = None
 
 
 def geo_ip_lookup(ip_address):
-    record = geoip_reader.city(ip_address)
-    if record is None:
-        return ("unknown")
-    return (record.country.iso_code)
+  record = geoip_reader.city(ip_address)
+  if record is None:
+      return ("unknown")
+  return (record.country.iso_code)
 
-if __name__ == '__main__':
+def check_circuit():
+  circuit_csv_filename = 'data/circuit.csv'
+
+# def check_circuit(date_of_consensus,time_of_consensus):
+#   circuit_csv_filename = 'data/%s-%s-all_node_info.csv' % (date_of_consensus,time_of_consensus)
+
+  csv = open(circuit_csv_filename, 'w+')
+
+  csv.write('Circuit ID,Circuit Purpose,Fingerprint,Nickname,IP,Country Origin\n')
+  print("  [+] Created CSV file: %s" % (circuit_csv_filename))
+
   geoip_reader = geoip2.database.Reader('/usr/share/GeoIP/%s' % GEOIP_FILENAME)
   
   with Controller.from_port(port = 9051) as controller:
@@ -39,4 +49,8 @@ if __name__ == '__main__':
         country = geo_ip_lookup(address)
 
 #         print(" %s- %s (%s, %s, %s)" % (div, fingerprint, nickname, address, country))
-        print("Circuit %s (%s) %s (%s, %s, %s)" % (circ.id, circ.purpose, fingerprint, nickname, address, country))
+#         print("Circuit %s (%s) %s (%s, %s, %s)" % (circ.id, circ.purpose, fingerprint, nickname, address, country))
+        csv_fill.write("%s,%s,%s,%s,%s,%s\n" % (circ.id, circ.purpose, fingerprint, nickname, address, country)
+                       
+if __name__ == '__main__':
+  check_circuit()
