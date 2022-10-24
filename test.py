71037a7db37c7934ff6c2878ea4a5e10049d6857
jackiezkl <jackiezkl@users.noreply.github.com>
2022-10-24 17:50:01 -0500

    Update test.py

--- test.py
+++ test.py
@@ -77,14 +77,20 @@ def change_circuit():
 if __name__ == '__main__':
   geoip_reader = geoip2.database.Reader('/usr/share/GeoIP/%s' % GEOIP_FILENAME)
   
-  
-  tor_proc = subprocess.Popen(['tor','-f','data/torrc'])
-  print('building tor circuit...')
-
-  time.sleep(20)
-  
+  proc = subprocess.Popen(['python3','circuit.py'],stdout=subprocess.PIPE)
   while True:
-    change_circuit()
-    time.sleep(1)
-    record_circuit()
-    time.sleep(1)
+    line = proc.stdout.readline()
+    if "Bootstrapped 100% (done): Done" in line.decode().rstrip():
+      while True:
+        change_circuit()
+        time.sleep(1)
+        record_circuit()
+        time.sleep(1)
+
+
+#   tor_proc = subprocess.Popen(['tor','-f','data/torrc'])
+#   print('building tor circuit...')
+
+#   time.sleep(20)
+  
+
