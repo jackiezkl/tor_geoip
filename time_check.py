65d130a6965686ebf0d1d5a33bab0e512ce497f9
jackiezkl <jackiezkl@users.noreply.github.com>
2022-10-25 11:47:45 -0500

    Update time_check.py

--- time_check.py
+++ time_check.py
@@ -2,14 +2,14 @@ import subprocess
 
 tor_proc = subprocess.Popen(['tor','-f','data/torrc'],stdout=subprocess.PIPE)
 print("  [+] Tor started in the background. Collecting circuit information now...")
-while True:
-  line = tor_proc.stdout.readline()
-  print(line.decode().rstrip())
-  if "Bootstrapped 100% (done): Done" in line.decode().rstrip():
-    try:
-      while True:
-        print("test 2")
-    except KeyboardInterrupt:
+# while True:
+line = tor_proc.stdout.readline()
+print(line.decode().rstrip())
+if "Bootstrapped 100% (done): Done" in line.decode().rstrip():
+  try:
+    while True:
+      print("test 2")
+  except KeyboardInterrupt:
       print("[+] Progress manually stopped, gracefully existing...")
 #         tor_proc.kill()
 #         sys.exit(0)
