eaae5682e48a6b16c72cf3766316459ce5f4d49f
jackiezkl <jackiezkl@users.noreply.github.com>
2022-10-24 21:53:29 -0500

    Update time_check.py

--- time_check.py
+++ time_check.py
@@ -1,4 +1,20 @@
-import time
+import datetime
 
-t = time.localtime()
-print(t)
+def check_time(time_of_consensus, collection_length):
+  hour = time_of_consensus.split("-")
+  int_hour = (int(hour[0])+collection_length)%24
+  q = datetime.datetime.now().hour
+  
+  
+  if int_hour == q:
+    tor_proc.kill()
+    sys.exit()
+    os._exit()
+  else:
+    continue
+  
+
+
+if __name__=="__main__":
+  while True:
+    check_time("02-00-00",1)
