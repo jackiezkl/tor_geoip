82f5d2292c209181fcd067a0a1c0ffb8e427745e
jackiezkl <jackiezkl@users.noreply.github.com>
2022-10-25 09:24:13 -0500

    Update time_check.py

--- time_check.py
+++ time_check.py
@@ -1,29 +1,29 @@
 from datetime import datetime
 import time
 
-def check_time(time_of_consensus, collection_length):
-  hour = time_of_consensus.split("-")
-  int_hour = (int(hour[0])+collection_length)%24
-  q = datetime.utcnow().hour
+# def check_time(time_of_consensus, collection_length):
+#   hour = time_of_consensus.split("-")
+#   int_hour = (int(hour[0])+collection_length)%24
+#   q = datetime.utcnow().hour
   
   
-  if int_hour == q:
-    print("got it")
-#     tor_proc.kill()
-    sys.exit()
-    os._exit()
+#   if int_hour == q:
+#     print("got it")
+# #     tor_proc.kill()
+#     sys.exit()
+#     os._exit()
 
 def check(i):
   if i == 10:
     return "F"
   else:
-    print(i)
     return "T"
     
 if __name__ == "__main_":
   i = 0
   l = "T"
   while l == "T":
+    print(i)
     l = check(i)
     i+=1
 # if __name__=="__main__":
