826b335517dd61f4ef55eb5ede71d39987a40095
jackiezkl <jackiezkl@users.noreply.github.com>
2022-10-25 10:32:47 -0500

    Update time_check.py

--- time_check.py
+++ time_check.py
@@ -43,21 +43,6 @@ def node_ping(path_to_file, which_node, date_of_consensus, time_of_consensus):
     node = 'E'
     ping_result_filename = 'data/'+date_of_consensus+'-'+time_of_consensus+'-ping_exit_result.csv'
 
-  does_file_exist = exists(ping_result_filename)
-  if does_file_exist == True:
-    while True:
-      over_write = input("  [+] The ping file already exist, overwrite? (y/n):")
-      if over_write.lower() == "n" or over_write.lower() == "no":
-        print("  [+] Will not over write file.")
-        return
-      elif over_write.lower() == "y" or over_write.lower() == "yes":
-        print("  [+] Overwriting file...")
-        break
-      else:
-        print("  [+] Please answer 'yes' or 'no'.")
-        continue
-
-
   result_fill = open(ping_result_filename, 'w+')
   result_fill.write('nickname,fingerprint,ip,latency\n')
 
@@ -83,6 +68,23 @@ def node_ping(path_to_file, which_node, date_of_consensus, time_of_consensus):
   result_fill.close()
   print("  [+] Done pinging %s! Please check file data/%s-%s-ping_%s_result.csv" % (which_node,date_of_consensus,
                                                                                     time_of_consensus,which_node))
+def over_write(which_node, date_of_consensus,time_of_consensus):
+  result_filepath = 'data/'+date_of_consensus+'-'+time_of_consensus+'-ping_'+which_node+'_result.csv'
+  file_exist = exists(result_filepath)
+  if file_exist == True:
+    while True:
+      over_write = input("  [+] The ping file already exist, overwrite? (y/n):")
+      if over_write.lower() == "n" or over_write.lower() == "no":
+        print("  [+] Will not over write file.")
+        return "no"
+        break
+      elif over_write.lower() == "y" or over_write.lower() == "yes":
+        print("  [+] Overwriting file...")
+        return "yes"
+        break
+      else:
+        print("  [+] Please answer 'yes' or 'no'.")
+        continue
 
 class pingThread (threading.Thread):
   def __init__(self, threadID, name, counter, file_path, node_option,date_of_consensus,time_of_consensus):
@@ -102,17 +104,18 @@ if __name__ == "__main__":
   date_of_consensus = "2022-10-25"
   time_of_consensus = "03-00-00"
   node_file_path = "data/2022-10-25-03-00-00-all_node_info.csv"
-  
-  guard_thread = pingThread(1, "ping guard", 1, node_file_path, "guard",date_of_consensus, time_of_consensus)
-  middle_thread = pingThread(2, "ping middle", 2, node_file_path, "middle",date_of_consensus, time_of_consensus)
-  exit_thread = pingThread(3, "ping exit", 3, node_file_path, "exit",date_of_consensus, time_of_consensus)
-  
-  exit_thread.start()
-  exit_thread.join()
 
-  guard_thread.start()
-  guard_thread.join()
+  if over_write("guard",date_of_consensus,time_of_consensus) == "yes):
+    guard_thread = pingThread(1, "ping guard", 1, node_file_path, "guard",date_of_consensus, time_of_consensus)
+    guard_thread.start()
+    guard_thread.join()
 
-  middle_thread.start()
-  middle_thread.join()
+  if over_write("middle",date_of_consensus,time_of_consensus) == "yes":
+    middle_thread = pingThread(2, "ping middle", 2, node_file_path, "middle",date_of_consensus, time_of_consensus)
+    middle_thread.start()
+    middle_thread.join()
 
+  if over_write("exit",date_of_consensus,time_of_consensus) == "yes":
+    exit_thread = pingThread(3, "ping exit", 3, node_file_path, "exit",date_of_consensus, time_of_consensus)
+    exit_thread.start()
+    exit_thread.join()
